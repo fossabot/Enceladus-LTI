7f56d65c3fab43230d5e2bdb53a16d88530a3471
Jacob Pratt <the.z.cuber@gmail.com>
2018-08-21 02:55:01 -0400

    Add favicons, fonts, manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,62 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'enceladus-lti',
+    title: 'Enceladus',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {
+        charset: 'utf-8',
+      }, {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      }, {
+        hid: 'description',
+        name: 'description',
+        content: 'Enceladus launch thread interface',
+      }, {
+        name: 'apple-mobile-web-app-title',
+        content: 'Enceladus',
+      }, {
+        name: 'application-name',
+        content: 'Enceladus',
+      }, {
+        name: 'msapplication-TitleColor',
+        content: '#000000',
+      }, {
+        name: 'theme-color',
+        content: '#ff5100',
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      }, {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      }, {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      }, {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      }, {
+        rel: 'mask-icon',
+        href: '/favicon/safari-pinned-tab.svg',
+        color: '#ff5100',
+      }
     ]
   },
+
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: false,
+
   /*
   ** Build configuration
   */
