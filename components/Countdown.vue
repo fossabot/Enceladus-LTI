2f493e74a25b10cd5cd2166deb7d9252d61e07b6
Jacob Pratt <the.z.cuber@gmail.com>
2018-08-22 01:48:36 -0400

    Add countdown component, fonts

---
+++ components/Countdown.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    {{ display_time }}
+  </section>
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'nuxt-class-component';
+
+function pad(n) {
+  return do {
+    if (n < 10) `0${n}`;
+    else n;
+  }
+}
+
+@Component
+export default class Countdown extends Vue {
+  t0 = null;
+  sign = '-';
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+
+  mounted() {
+    this.interval = setInterval(this.update_clock, 1000);
+  }
+
+  get display_time() {
+    return do {
+      if (this.hours >= 48) {
+        `T${this.sign}${Math.floor(this.hours / 24)}d ${this.hours % 24}h`;
+      } else {
+        `T${this.sign}${pad(this.hours)}:${pad(this.minutes)}:${pad(this.seconds)}`;
+      }
+    };
+  }
+
+  update_clock() {
+    this.sign = do {
+      if (this.t0 > Date.now() / 1000) '-';
+      else '+';
+    };
+
+    const diff = Math.abs(Math.floor(this.t0 - Date.now() / 1000));
+
+    this.hours = Math.floor(diff / 3600);
+    this.minutes = Math.floor(diff % 3600 / 60);
+    this.seconds = diff % 60;
+  }
+}
+</script>
+
+<style lang='postcss' scoped>
+section {
+  color: lch(100 0 0);
+  font-size: 17px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <main>
     <logo/>
-    <div class='header'/>
+    <div class='header'>
+      <countdown/>
+    </div>
     <div class='links'/>
     <div class='youtube'/>
     <div class='updates'/>
@@ -12,15 +14,24 @@
 
 <script>
 import Logo from '~/components/Logo.vue';
+import Countdown from '~/components/Countdown.vue';
 
 export default {
   components: {
     Logo,
+    Countdown,
   }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: 'D-DIN';
+  src:
+    url('~/fonts/woff2/D-DIN-expanded.woff2') format('woff2'),
+    url('~/fonts/woff/D-DIN-expanded.woff') format('woff');
+}
+
 main {
   --theme-color: lch(60 97 48);
 
@@ -40,14 +51,24 @@ main {
     ' sections  youtube  twitter ' calc((100vw - $gap * ($number-columns + 1)) * $youtube-ratio * (9 / 16))
     ' sections  updates  twitter ' auto
     / 1fr 2fr 1fr;
+
+  font-family: 'D-DIN';
 }
 
-.header { grid-area: header; }
 .links { grid-area: links; }
 .youtube { grid-area: youtube; }
 .updates { grid-area: updates; }
 .sections { grid-area: sections; }
 .twitter { grid-area: twitter; }
+
+.header {
+  grid-area: header;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
 
 main > div {
   border: 1px solid lch(100 0 0);
